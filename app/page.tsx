09287a898150cc66550cc4343c98c4adfa43b887
rexxy04 <muhammadirgi088@gmail.com>
2025-11-22 15:33:22 +0800

    Refactoring navbar and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,12 @@
-import Navbar from "../components/layout/Navbar";
 import HeroSection from "../components/home/HeroSection";
 import FeaturedSection from "../components/home/FeaturedSection";
-import Footer from "../components/layout/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50">
-      {/* 1. Navbar Global */}
-      <Navbar />
-      
-      {/* 2. Hero Section */}
+    <main>
       <HeroSection />
 
-      {/* 3. Featured Section (Bento Grid) */}
       <FeaturedSection />
-
-      {/* 4. Footer (Bagian paling bawah) */}
-      <Footer />
 
     </main>
   );
